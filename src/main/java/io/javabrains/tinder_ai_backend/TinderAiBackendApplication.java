bd44b1f24a2dea95b49337914a79cb414cca096e
avirupmallik <aviruptatai.mallik@gmail.com>
2024-07-22 19:35:59 -0500

    Commit profile creation service

--- src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/io/javabrains/tinder_ai_backend/TinderAiBackendApplication.java
@@ -13,7 +13,6 @@ import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
@@ -35,6 +34,7 @@ public class TinderAiBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		ChatResponse response = chatModel.call(
 				new Prompt(
 						"Generate the names of 5 famous pirates.",
